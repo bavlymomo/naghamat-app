00f5c94fc6cc7ac031ac46ca37420cc3062343bf
bavlymomo <bavlymomtaz10@gmail.com>
2025-09-16 14:10:12 +0300

    make the list of buttons for rings

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MusicWidget());
+  runApp(MusicWidget());
 }
 
-class MusicWidget extends StatelessWidget {
-  const MusicWidget({super.key});
+class MusicWidget extends StatefulWidget {
+  MusicWidget({super.key});
+
+  @override
+  State<MusicWidget> createState() => MusicWidgetState();
+}
+
+class MusicWidgetState extends State<MusicWidget> {
+  final List<Map<String, dynamic>> Rings = [
+    {"file": "music-1.mp3", "label": "Nokia", "color": Colors.red},
+    {"file": "music-2.mp3", "label": "Samsung", "color": Colors.blue},
+    {"file": "music-3.mp3", "label": "Laptop", "color": Colors.green},
+    {"file": "music-4.mp3", "label": "iPhone", "color": Colors.purple},
+    {"file": "music-5.mp3", "label": "Old Phone", "color": Colors.orange},
+    {"file": "music-6.mp3", "label": "Digital Tone", "color": Colors.teal},
+    {"file": "music-7.mp3", "label": "Classic", "color": Colors.black},
+  ];
+
+  final AudioPlayer play = AudioPlayer();
+
+  playAudio(String source) async {
+   
+    await play.play(AssetSource(source));
+  }
+
+  stopAudio() async {
+    await play.stop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +43,38 @@ class MusicWidget extends StatelessWidget {
           title: Text("Naghamat", style: TextStyle(color: Colors.white)),
           backgroundColor: Color(0xff8902A2),
         ),
-        body: Column(
-          children: [
-            TextButton(
-              onPressed: () {},
-              child: Expanded(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(color: Colors.white),
-                  child: Text("Apple RingTone"),
-                ),
-              ),
+        body:
+           
+            ListView.builder(
+              itemCount: Rings.length,
+              itemBuilder: (context, index) {
+                Map ring = Rings[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ElevatedButton(
+                    onPressed: () => playAudio(ring["file"]),
+                    style: ButtonStyle(
+                      shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(12),
+                        ),
+                      ),
+                      fixedSize: WidgetStatePropertyAll<Size>(
+                        Size(MediaQuery.of(context).size.width, 100),
+                      ),
+                    ),
+                    child: Text(
+                      ring["label"],
+                      style: TextStyle(
+                        color: ring["color"],
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
       ),
     );
   }
