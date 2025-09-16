26d6945e7d760346160e69b9cd2d73f88eff64a2
bavlymomo <bavlymomtaz10@gmail.com>
2025-09-16 08:27:43 +0300

    add assets folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MusicWidget());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MusicWidget extends StatelessWidget {
+  const MusicWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.purple[300],
         appBar: AppBar(
           title: Text("Naghamat", style: TextStyle(color: Colors.white)),
           backgroundColor: Color(0xff8902A2),
         ),
-        body: Center(),
+        body: Column(
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: Expanded(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Text("Apple RingTone"),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
